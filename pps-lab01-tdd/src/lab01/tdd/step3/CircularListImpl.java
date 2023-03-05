ac2868f6de411fac06625668168d71dc7f3f5744
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2023-03-05 17:43:35 +0100

    test empty filteredNext

---
+++ pps-lab01-tdd/src/lab01/tdd/step3/CircularListImpl.java
@@ -0,0 +1,36 @@
+package lab01.tdd.step3;
+
+import lab01.tdd.step2.BackwardIterator;
+import lab01.tdd.step2.ForwardIterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+public class CircularListImpl implements CircularList {
+
+    private final List<Integer> circularList = new ArrayList<>();
+
+    @Override
+    public void add(int element) {
+        this.circularList.add(element);
+    }
+
+    @Override
+    public int size() {
+        return this.circularList.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.circularList.isEmpty();
+    }
+
+    @Override
+    public Optional<Integer> filteredNext(Predicate<? super Integer> predicate) {
+        return this.isEmpty() ? Optional.empty() : this.circularList.stream().filter(predicate).findFirst();
+    }
+
+}
---
+++ pps-lab01-tdd/test/lab01/tdd/step3/CircularListTest.java
@@ -0,0 +1,27 @@
+package lab01.tdd.step3;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CircularListTest {
+
+    private CircularList circularList;
+
+    @BeforeEach
+    void beforeEach() {
+        this.circularList = new CircularListImpl();
+    }
+
+    @Test
+    void testEmptyFilteredNext() {
+        this.circularList.add(0);
+        this.circularList.add(1);
+        this.circularList.add(2);
+        assertEquals(Optional.empty(), this.circularList.filteredNext(i -> i >= 3));
+    }
+
+}
